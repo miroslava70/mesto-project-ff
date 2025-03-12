693d3047cc7471dd5535c8fc77f0726a0dfc01e0
Мирослава Зимонина <zimoninamiroslava@yandex.ru>
2025-03-12 10:36:28 +0700

    Javascript код картинок для сайта "Место"

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,48 @@
-// @todo: Темплейт карточки
-
 // @todo: DOM узлы
+const content = document.querySelector('.content');
+const places = content.querySelector('.places__list')
+const popupButton = content.querySelector('.profile__add-button');
+const popup = document.querySelector('.popup_type_new-card');
+const addButton = popup.querySelector('.popup__button');
+const closePopup = popup.querySelector('.popup__close');
+
+// @todo: Popup
+popupButton.addEventListener('click', function () {
+    popup.classList.add('popup_is-opened');
+});
+
+closePopup.addEventListener('click', function () {
+    popup.classList.remove('popup_is-opened');
+});
 
 // @todo: Функция создания карточки
+function addImages(imageLink, NameValue) {
+    // @todo: Темплейт карточки
+    const cardTemplate = document.querySelector('#card-template').content;
+    const card = cardTemplate.querySelector('.card').cloneNode(true);
 
-// @todo: Функция удаления карточки
+    const likeButton = card.querySelector('.card__like-button');
+    const deleteButton = card.querySelector('.card__delete-button');
+
+    card.querySelector('.card__image').src = imageLink;
+    card.querySelector('.card__title').textContent = NameValue;
+
+    // @todo: Функция удаления карточки
+    deleteButton.addEventListener('click', function (e) {
+        console.log(e.target);
+        card.remove();
+    });
+
+    likeButton.addEventListener('click', function (evt) {
+        const eventTarget = evt.target;
+        console.log(eventTarget)
+        eventTarget.classList.toggle('card__like-button_is-active');
+    });
+
+    places.append(card);
+};
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach(function (element) {
+    addImages(element.link, element.name);
+});
